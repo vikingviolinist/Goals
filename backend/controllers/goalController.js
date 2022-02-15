9e84d1d4cb7c5e1a68fc0333a00ff61ee1f11fa0
Michal Petrik <misopetrik@gmail.com>
2022-02-15 18:11:46 +0100

    :sparkles: Initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,61 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
+
 // @desc    Get goals
 // @route   GET /api/goals
-// @accnss  Private
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get goals' });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc    Set goal
 // @route   POST /api/goals
-// @accnss  Private
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
+  const { id } = req.params;
   const { text } = req.body;
   if (!text) {
     res.status(400);
     throw new Error('Please add a text field');
   }
-  res.status(200).json({ message: `New goal set: ${text}` });
+
+  const goal = await Goal.create({ text });
+  res.status(200).json(goal);
 });
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
-// @accnss  Private
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const { id } = req.params;
+  const goal = await Goal.findById(id);
+  const { text } = req.body;
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(id, { text });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
-// @accnss  Private
+// @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const { id } = req.params;
+  const goal = await Goal.findByIdAndDelete(id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  res.status(200).json(id);
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
